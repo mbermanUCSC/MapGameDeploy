b171ef0d1e31548d26cd0a14ceecf96db4a588a2
mbermanUCSC <mhberman@ucsc.edu>
2024-03-17 23:06:32 -0700

    Revamp!

--- assets/script.js
+++ assets/script.js
@@ -11,84 +11,40 @@ document.addEventListener('scroll', function() {
     });
 });
 
+// print when a team member is clicked
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.team-member').forEach(function(teamMember) {
+        teamMember.addEventListener('click', function() {
+            var infoText = teamMember.getAttribute('data-info');
+
+            // split the text by the comma
+            var infoArray = infoText.split(',');
+
+            // set as list in class 'member-info'
+            var memberInfo = document.querySelector('.member-info');
+            memberInfo.innerHTML = '';
+            infoArray.forEach(function(info) {
+                var listItem = document.createElement('li');
+                listItem.innerHTML = info;
+                memberInfo.appendChild(listItem);
+            });
+
+
+            // set the button to a darker color
+            teamMember.style.backgroundColor = '#323';
+            // reset the color of the other buttons
+            document.querySelectorAll('.team-member').forEach(function(member) {
+                if (member !== teamMember) {
+                    member.style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+                }
+            });
+        });
+    });
+});
+
+
 
 // set up download section on page load
 document.addEventListener('DOMContentLoaded', function() {
     window.scrollTo(0, 1);
-    var downloadButton = document.querySelector('#downloadButton');
-    var platformSelect = document.querySelector('#platformSelect');
-    var platform = navigator.platform.toLowerCase();
-    var userAgent = navigator.userAgent.toLowerCase();
-
-    // set initial dropdown value
-    function setInitialDropdownValue() {
-        if (platform.includes('win')) {
-            platformSelect.value = 'windows';
-        } else if (platform.includes('mac')) {
-            platformSelect.value = 'mac';
-        } else if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(userAgent)) {
-            platformSelect.value = 'mobile';
-        } else {
-            platformSelect.value = 'windows'; // default
-        }
-    }
-
-    // update download button text and link
-    function updateDownloadButton() {
-        var selectedPlatform = platformSelect.value;
-        switch (selectedPlatform) {
-            case 'windows':
-                downloadButton.textContent = 'Download'; // windows
-                downloadButton.setAttribute('onclick', "location.href='path/to/windows/game.exe';");
-                break;
-            case 'mac':
-                downloadButton.textContent = 'Download'; // mac
-                downloadButton.setAttribute('onclick', "location.href='path/to/mac/game.dmg';");
-                break;
-            case 'mobile':
-                downloadButton.textContent = 'Download'; // mobile
-                downloadButton.setAttribute('onclick', "location.href='path/to/mobile/game.apk';");
-                break;
-            default:
-                downloadButton.textContent = 'Download';
-                downloadButton.removeAttribute('onclick');
-                break;
-        }
-    }
-
-    setInitialDropdownValue();
-    updateDownloadButton();
-    platformSelect.addEventListener('change', updateDownloadButton);
-
-});
-
-
-// scene slideshow functionality
-let slideIndex = 0; 
-let timer = null; 
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("sceneSlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    
-    clearTimeout(timer); // reset the timer
-    timer = setTimeout(showSlides, 5000); // change image every 5 seconds
-}
-
-
-function plusSlides(n) {
-    slideIndex += n - 1;
-    clearTimeout(timer); 
-    showSlides(); 
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    showSlides();
 });
